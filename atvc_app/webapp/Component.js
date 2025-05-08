1c9ae13d0c661373a30effaeb11aacf59e0ce8b1
Syed Farooq <syfarooq@deloitte.com>
2025-05-08 17:46:07 +0000

    commit1

---
+++ atvc_app/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("atvcapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
